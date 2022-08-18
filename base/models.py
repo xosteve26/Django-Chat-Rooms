0e900891c7605a2b595ef6901b1fe997d56b8033
Stephen George <stephengeorge2640@gmail.com>
2022-08-18 19:27:22 +0530

    Add host and topic field in models of Room

--- base/models.py
+++ base/models.py
@@ -1,9 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Topic(models.Model):
+    name=models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return self.name
 class Room(models.Model):
-    #host
-    #topic
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic=models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name=models.CharField(max_length=255)
     description=models.TextField(null=True, blank=True)
     #participants=
@@ -15,7 +21,7 @@ class Room(models.Model):
 
 
 class Message(models.Model):
-    #user=
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
     room=models.ForeignKey(Room, on_delete=models.CASCADE)
     body=models.TextField()
     created=models.DateTimeField(auto_now_add=True)
