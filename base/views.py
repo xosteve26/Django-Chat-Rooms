e7287cb7d87cf79a5b020952fc43033be21f8f75
Stephen George <stephengeorge2640@gmail.com>
2022-08-19 13:03:41 +0530

    Add create and update view and creation of form file

--- base/views.py
+++ base/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Room
+from .forms import RoomForm
 # Create your views here.
 
 rooms=None
@@ -16,3 +17,25 @@ def room(request, room_id):
     room=Room.objects.get(id=room_id)
     payload={'room':room}
     return render(request, 'base/room.html', payload)
+
+
+def create_room(request):
+    form=RoomForm()
+    if request.method == 'POST':
+        form=RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    payload={'form':form}
+    return render(request, 'base/room_form.html', payload)
+
+def edit_room(request, room_id):
+    room=Room.objects.get(id=room_id)
+    form=RoomForm(instance=room)
+    if request.method == 'POST':
+        form=RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    payload={'form':form}
+    return render(request, 'base/room_form.html', payload)
