8f796a51a26498d5990ac5532ccc3794cb3758e6
Stephen George <stephengeorge2640@gmail.com>
2022-08-19 19:51:18 +0530

    Create login logout routes + Route protection decorator

--- base/urls.py
+++ base/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('login', views.login_page, name='login'),
     path('', views.home, name='home'),
     path('room/<str:room_id>', views.room, name='room'),
     path('create_room', views.create_room, name='create_room'),
     path('edit_room/<str:room_id>', views.edit_room, name='edit_room'),
     path('delete_room/<str:room_id>', views.delete_room, name='delete_room'),
+    path('logout', views.logout_page, name='logout'),
+    
 ]
--- base/views.py
+++ base/views.py
@@ -1,14 +1,49 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-from .models import Room
+from django.contrib.auth import authenticate, login, logout
+from .models import Room, Topic, User
 from .forms import RoomForm
+from django.db.models import Q
 # Create your views here.
 
 rooms=None
 
+def login_page(request):
+    if request.method == "POST":
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+
+        try:
+            user=User.objects.get(username=username)
+        except:
+            messages.error(request,'User does not exist')
+
+        user=authenticate(request,username=username, password=password)
+
+        if user:
+            login(request, user)
+            return redirect('/')
+        else:
+            messages.error(request,'Invalid credentials')
+            return redirect('/login')
+
+    return render(request, 'base/login_register.html')
+
 def home(request):
-    rooms=Room.objects.all()
-    payload={'rooms':rooms}
+    params=request.GET.get('q')
+    if params:
+        rooms=Room.objects.filter(
+            Q(topic__name__icontains=params) |
+            Q(name__icontains=params) | 
+            Q(description__icontains=params)
+        )
+    else:
+        rooms=Room.objects.all()
+    topics=Topic.objects.all()
+    count=rooms.count()
+    payload={'rooms':rooms, 'topics':topics,'room_count':count}
     
     return render(request, 'base/home.html', payload)
 
@@ -18,7 +53,7 @@ def room(request, room_id):
     payload={'room':room}
     return render(request, 'base/room.html', payload)
 
-
+@login_required(login_url='/login')
 def create_room(request):
     form=RoomForm()
     if request.method == 'POST':
@@ -29,6 +64,8 @@ def create_room(request):
     payload={'form':form}
     return render(request, 'base/room_form.html', payload)
 
+
+@login_required(login_url='/login')
 def edit_room(request, room_id):
     room=Room.objects.get(id=room_id)
     form=RoomForm(instance=room)
@@ -39,7 +76,9 @@ def edit_room(request, room_id):
             return redirect('/')
     payload={'form':form}
     return render(request, 'base/room_form.html', payload)
+    
 
+@login_required(login_url='/login')
 def delete_room(request, room_id):
     room = Room.objects.get(id=room_id)
     if request.method == "POST":
@@ -47,3 +86,10 @@ def delete_room(request, room_id):
         return redirect('/')
 
     return render(request, 'base/delete.html',{'obj':room.name})
+
+def logout_page(request):
+    logout(request)
+    return redirect('/')
+
+
+
